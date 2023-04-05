3c34a66d6396502230e48c39057b09d027134260
faja rivaldi chan <fajarrivaldi2015@gmail.com>
2023-04-05 23:40:41 +0700

    penambahan alternatif

---
+++ app/Http/Controllers/KriteriaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kriteria;
+use Illuminate\Http\Request;
+
+class KriteriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+}
